5f27cdfb1827599d6d32cef1ced7bf84aecbb0ad
Liksu <github@Liksu.kiev.ua>
2021-12-24 01:46:45 +0200

    Add all tags

---
+++ tags.js
@@ -0,0 +1,417 @@
+import createElement from "./index.js";
+
+export function a(attributes, children) {
+    return createElement('A', attributes, children)
+}
+
+export function abbr(attributes, children) {
+    return createElement('ABBR', attributes, children)
+}
+
+export function address(attributes, children) {
+    return createElement('ADDRESS', attributes, children)
+}
+
+export function area(attributes, children) {
+    return createElement('AREA', attributes, children)
+}
+
+export function article(attributes, children) {
+    return createElement('ARTICLE', attributes, children)
+}
+
+export function aside(attributes, children) {
+    return createElement('ASIDE', attributes, children)
+}
+
+export function audio(attributes, children) {
+    return createElement('AUDIO', attributes, children)
+}
+
+export function b(attributes, children) {
+    return createElement('B', attributes, children)
+}
+
+export function base(attributes) {
+    return createElement('BASE', attributes)
+}
+
+export function bdo(attributes, children) {
+    return createElement('BDO', attributes, children)
+}
+
+export function blockquote(attributes, children) {
+    return createElement('BLOCKQUOTE', attributes, children)
+}
+
+export function body(attributes, children) {
+    return createElement('BODY', attributes, children)
+}
+
+export function br() {
+    return createElement('BR')
+}
+
+export function button(attributes, children) {
+    return createElement('BUTTON', attributes, children)
+}
+
+export function canvas(attributes) {
+    return createElement('CANVAS', attributes)
+}
+
+export function caption(attributes, children) {
+    return createElement('CAPTION', attributes, children)
+}
+
+export function cite(attributes, children) {
+    return createElement('CITE', attributes, children)
+}
+
+export function code(attributes, children) {
+    return createElement('CODE', attributes, children)
+}
+
+export function col(attributes, children) {
+    return createElement('COL', attributes, children)
+}
+
+export function colgroup(attributes, children) {
+    return createElement('COLGROUP', attributes, children)
+}
+
+export function command(attributes, children) {
+    return createElement('COMMAND', attributes, children)
+}
+
+export function datalist(attributes, children) {
+    return createElement('DATALIST', attributes, children)
+}
+
+export function dd(attributes, children) {
+    return createElement('DD', attributes, children)
+}
+
+export function del(attributes, children) {
+    return createElement('DEL', attributes, children)
+}
+
+export function details(attributes, children) {
+    return createElement('DETAILS', attributes, children)
+}
+
+export function dfn(attributes, children) {
+    return createElement('DFN', attributes, children)
+}
+
+export function div(attributes, children) {
+    return createElement('DIV', attributes, children)
+}
+
+export function dl(attributes, children) {
+    return createElement('DL', attributes, children)
+}
+
+export function dt(attributes, children) {
+    return createElement('DT', attributes, children)
+}
+
+export function em(attributes, children) {
+    return createElement('EM', attributes, children)
+}
+
+export function embed(attributes, children) {
+    return createElement('EMBED', attributes, children)
+}
+
+export function fieldset(attributes, children) {
+    return createElement('FIELDSET', attributes, children)
+}
+
+export function figcaption(attributes, children) {
+    return createElement('FIGCAPTION', attributes, children)
+}
+
+export function figure(attributes, children) {
+    return createElement('FIGURE', attributes, children)
+}
+
+export function footer(attributes, children) {
+    return createElement('FOOTER', attributes, children)
+}
+
+export function form(attributes, children) {
+    return createElement('FORM', attributes, children)
+}
+
+export function h1(attributes, children) {
+    return createElement('H1', attributes, children)
+}
+
+export function h2(attributes, children) {
+    return createElement('H2', attributes, children)
+}
+
+export function h3(attributes, children) {
+    return createElement('H3', attributes, children)
+}
+
+export function h4(attributes, children) {
+    return createElement('H4', attributes, children)
+}
+
+export function h5(attributes, children) {
+    return createElement('H5', attributes, children)
+}
+
+export function h6(attributes, children) {
+    return createElement('H6', attributes, children)
+}
+
+export function head(attributes, children) {
+    return createElement('HEAD', attributes, children)
+}
+
+export function header(attributes, children) {
+    return createElement('HEADER', attributes, children)
+}
+
+export function hr(attributes) {
+    return createElement('HR', attributes)
+}
+
+export function html(attributes, children) {
+    return createElement('HTML', attributes, children)
+}
+
+export function i(attributes, children) {
+    return createElement('I', attributes, children)
+}
+
+export function iframe(attributes, children) {
+    return createElement('IFRAME', attributes, children)
+}
+
+export function img(attributes) {
+    return createElement('IMG', attributes)
+}
+
+export function input(attributes) {
+    return createElement('INPUT', attributes)
+}
+
+export function ins(attributes, children) {
+    return createElement('INS', attributes, children)
+}
+
+export function kbd(attributes, children) {
+    return createElement('KBD', attributes, children)
+}
+
+export function label(attributes, children) {
+    return createElement('LABEL', attributes, children)
+}
+
+export function legend(attributes, children) {
+    return createElement('LEGEND', attributes, children)
+}
+
+export function li(attributes, children) {
+    return createElement('LI', attributes, children)
+}
+
+export function link(attributes, children) {
+    return createElement('LINK', attributes, children)
+}
+
+export function map(attributes, children) {
+    return createElement('MAP', attributes, children)
+}
+
+export function mark(attributes, children) {
+    return createElement('MARK', attributes, children)
+}
+
+export function menu(attributes, children) {
+    return createElement('MENU', attributes, children)
+}
+
+export function meta(attributes, children) {
+    return createElement('META', attributes, children)
+}
+
+export function meter(attributes, children) {
+    return createElement('METER', attributes, children)
+}
+
+export function nav(attributes, children) {
+    return createElement('NAV', attributes, children)
+}
+
+export function noscript(attributes, children) {
+    return createElement('NOSCRIPT', attributes, children)
+}
+
+export function object(attributes, children) {
+    return createElement('OBJECT', attributes, children)
+}
+
+export function ol(attributes, children) {
+    return createElement('OL', attributes, children)
+}
+
+export function optgroup(attributes, children) {
+    return createElement('OPTGROUP', attributes, children)
+}
+
+export function option(attributes, children) {
+    return createElement('OPTION', attributes, children)
+}
+
+export function output(attributes, children) {
+    return createElement('OUTPUT', attributes, children)
+}
+
+export function p(attributes, children) {
+    return createElement('P', attributes, children)
+}
+
+export function param(attributes, children) {
+    return createElement('PARAM', attributes, children)
+}
+
+export function pre(attributes, children) {
+    return createElement('PRE', attributes, children)
+}
+
+export function progress(attributes, children) {
+    return createElement('PROGRESS', attributes, children)
+}
+
+export function q(attributes, children) {
+    return createElement('Q', attributes, children)
+}
+
+export function rp(attributes, children) {
+    return createElement('RP', attributes, children)
+}
+
+export function rt(attributes, children) {
+    return createElement('RT', attributes, children)
+}
+
+export function ruby(attributes, children) {
+    return createElement('RUBY', attributes, children)
+}
+
+export function s(attributes, children) {
+    return createElement('S', attributes, children)
+}
+
+export function samp(attributes, children) {
+    return createElement('SAMP', attributes, children)
+}
+
+export function script(attributes, children) {
+    return createElement('SCRIPT', attributes, children)
+}
+
+export function section(attributes, children) {
+    return createElement('SECTION', attributes, children)
+}
+
+export function select(attributes, children) {
+    return createElement('SELECT', attributes, children)
+}
+
+export function small(attributes, children) {
+    return createElement('SMALL', attributes, children)
+}
+
+export function source(attributes, children) {
+    return createElement('SOURCE', attributes, children)
+}
+
+export function span(attributes, children) {
+    return createElement('SPAN', attributes, children)
+}
+
+export function strong(attributes, children) {
+    return createElement('STRONG', attributes, children)
+}
+
+export function style(attributes, children) {
+    return createElement('STYLE', attributes, children)
+}
+
+export function sub(attributes, children) {
+    return createElement('SUB', attributes, children)
+}
+
+export function sup(attributes, children) {
+    return createElement('SUP', attributes, children)
+}
+
+export function table(attributes, children) {
+    return createElement('TABLE', attributes, children)
+}
+
+export function tbody(attributes, children) {
+    return createElement('TBODY', attributes, children)
+}
+
+export function td(attributes, children) {
+    return createElement('TD', attributes, children)
+}
+
+export function textarea(attributes, children) {
+    return createElement('TEXTAREA', attributes, children)
+}
+
+export function tfoot(attributes, children) {
+    return createElement('TFOOT', attributes, children)
+}
+
+export function th(attributes, children) {
+    return createElement('TH', attributes, children)
+}
+
+export function thead(attributes, children) {
+    return createElement('THEAD', attributes, children)
+}
+
+export function time(attributes, children) {
+    return createElement('TIME', attributes, children)
+}
+
+export function title(attributes, children) {
+    return createElement('TITLE', attributes, children)
+}
+
+export function tr(attributes, children) {
+    return createElement('TR', attributes, children)
+}
+
+export function track(attributes, children) {
+    return createElement('TRACK', attributes, children)
+}
+
+export function u(attributes, children) {
+    return createElement('U', attributes, children)
+}
+
+export function ul(attributes, children) {
+    return createElement('UL', attributes, children)
+}
+
+export function variable(attributes, children) {
+    return createElement('VAR', attributes, children)
+}
+
+export function video(attributes, children) {
+    return createElement('VIDEO', attributes, children)
+}
+
+export function wbr(attributes, children) {
+    return createElement('WBR', attributes, children)
+}
